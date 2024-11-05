35f7ff77f44af55549dec190fd62e7a7e9457a98
Hanzhe-Angela <73732316+Hanzhe-Angela@users.noreply.github.com>
2024-11-04 20:22:49 -0800

    tried firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 
@@ -38,8 +39,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.database)
+    implementation(libs.firebase.functions)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation("mysql:mysql-connector-java:8.0.30")
 }
---
+++ app/src/main/java/com/example/ginshinimpact_project2_cs310/FirstFragment.java
@@ -0,0 +1,8 @@
+package com.example.ginshinimpact_project2_cs310;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.BuildConfig;
+import androidx.fragment.app.Fragment;
+
+public class FirstFragment extends Fragment {
+
+}
